9dd2445bd1893ba8806de31805bf16098e5d4193
Sriinnu <hello@srinivas.dev>
2026-07-03 13:50:01 +0200

    runic 2.5.0

--- Casks/runic.rb
+++ Casks/runic.rb
@@ -1,6 +1,6 @@
 cask "runic" do
-  version "2.4.1"
-  sha256 "cb279ca3b91fd94c24e5b9a86c1bf7713ff454456636110a838721ebf1ff1e81"
+  version "2.5.0"
+  sha256 "ca4e706532764a64e01880395527e5f27d9b7ead1e16c4f495e942e4a71a94db"
 
   url "https://github.com/sriinnu/Runic/releases/download/v#{version}/Runic-#{version}.zip"
   name "Runic"
